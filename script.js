b465cb9f4c1b36da348cec9940d18efd7d1f5ad8
odongotim <timothyodongo263@gmail.com>
2026-02-22 13:45:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -236,3 +236,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 });
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js")
+    .then(() => console.log("Service Worker Registered"));
+}
